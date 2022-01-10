d35feb79b398e6843c61773ef39b5156193ade48
stateout <73503098+stateout@users.noreply.github.com>
2022-01-10 11:21:08 -0500

    Update SQLQuery_2.update-table.sql

--- SQLQuery_2.update-table.sql
+++ SQLQuery_2.update-table.sql
@@ -1,5 +1,12 @@
-UPDATE SUVTruckInfo
-SET Vehicle_Class='Mid-size' 
-WHERE Model='Grand Cherokee' AND Automaker='Jeep'
+-- Querying the table SUVTruckinfo where information about vehicle make, vehicle type and class, model, and price can be found
 
 SELECT * FROM SUVTruckInfo
+
+-- During data cleaning step, inaccurate data had been found in the Vehicle Class column for the Automaker: Jeep; and Model: Grand Cherokee. To fix this,
+the following code was used
+
+UPDATE SUVTruckInfo
+SET Vehicle_Class='Mid-size'
+WHERE Automaker = 'Jeep' AND Model = 'Grand Cherokee'
+
+-- 
