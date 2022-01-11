c582b937a650a1742bcfbdc1cbbd302aa456e344
stateout <73503098+stateout@users.noreply.github.com>
2022-01-10 19:51:41 -0500

    Update SQLQuery_2.update-table.sql

--- SQLQuery_2.update-table.sql
+++ SQLQuery_2.update-table.sql
@@ -1,5 +1,9 @@
+-- To query the data set created --
+
+SELECT * FROM SUVTruckInfo
+
+-- To correct for inaccurate data found under the Vehicle Class column found for the Jeep Grand Cherokee, the following querying was used --
+
 UPDATE SUVTruckInfo
 SET Vehicle_Class='Mid-size' 
 WHERE Model='Grand Cherokee' AND Automaker='Jeep'
-
-SELECT * FROM SUVTruckInfo
