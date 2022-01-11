a715f610928e3a25a49275db75c3bb7a50c7bfb2
stateout <73503098+stateout@users.noreply.github.com>
2022-01-11 18:12:51 -0500

    Update SQLQuery_1.sql

--- SQLQuery_1.sql
+++ SQLQuery_1.sql
@@ -1,1 +1,10 @@
+-- Basic selecting the dataset table that had been imported into the database -- 
+
 SELECT * FROM SUVTruckInfo
+
+-- Correcting inaccurate data where the Jeep Grand Cherokee was mistakenly classified as a compact SUV instead of mid-size -- 
+
+UPDATE SUVTruckInfo
+SET Vehicle_Class = 'Mid-size' 
+WHERE Automaker = 'Jeep' AND Model = '%Grand Cherokee%'
+
