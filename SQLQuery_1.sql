da9e29b2884ffe8beab3ddfd8a5aa4bbc7fef488
stateout <73503098+stateout@users.noreply.github.com>
2022-01-23 20:42:59 -0500

    Update SQLQuery_1.sql

--- SQLQuery_1.sql
+++ SQLQuery_1.sql
@@ -22,6 +22,15 @@ UPDATE VehiclePriceComparison.dbo.SUVTruckInfo
 SET Vehicle_Class = 'Mid-size'
 WHERE Automaker LIKE 'JE%' AND Model LIKE 'Grand%'
 
+-- Query used to insert a new row of data into the existing table --
+
+INSERT INTO VehiclePriceComparison.dbo.SUVTruckInfo
+(Automaker, Vehicle_Type, Vehicle_Class, Model, Year, Trim, Base_Price, Fuel_Type)
+VALUES 
+('Ram', 'Truck', 'Full-size','1500','2022','Big Horn','60685','Gasoline')
+
+/* This query was used because of incomplete data. There was no data on Ram trucks despite having all other pick-up truck data available. */
+
 -- Query used to find the top 5 cheapest vehicles in the dataset -- 
 
 SELECT TOP(5) * FROM VehiclePriceComparison.dbo.SUVTruckInfo
@@ -33,18 +42,9 @@ ORDER BY Base_Price ASC
 -- Query used to find all American automakers listed in the database --
 
 SELECT * FROM VehiclePriceComparison.dbo.SUVTruckInfo
-WHERE Automaker ='Ford' OR Automaker = 'Chevrolet' OR Automaker = 'Dodge' OR Automaker = 'Jeep'
+WHERE Automaker IN ('Ford', 'Chevrolet', 'Dodge', 'Ram', 'Jeep')
 
--- Query used to insert a new row of data into the existing table --
-
-INSERT INTO VehiclePriceComparison.dbo.SUVTruckInfo
-(Automaker, Vehicle_Type, Vehicle_Class, Model, Year, Trim, Base_Price, Fuel_Type)
-VALUES 
-('Ram', 'Truck', 'Full-size','1500','2022','Big Horn','60685','Gasoline')
-
-/* This query was used because of incomplete data. There was no data on Ram trucks despite having all other pick-up truck data available. */
-
--- Query was used to full in an empty column called Fuel_Type --
+-- Query was used to fill in an empty column called Fuel_Type --
 
 UPDATE VehiclePriceComparison.dbo.SUVTruckInfo
 SET Fuel_Type = 'Gasoline' 
@@ -55,7 +55,16 @@ However, if for example the Ford Escape 2022 SEL were Hybrid, we would use the f
 
 UPDATE VehiclePriceComparison.dbo.SUVTruckInfo
 SET Fuel_Type = 'Hybrid' 
-WHERE Model = 'Escape' 
+WHERE Model = 'Escape' AND Trim = 'SEL' AND Year = '2022'
+
+-- Now to create a new table that provides information about which countries each automaker belongs to we would use the following query --
+
+CREATE TABLE VehiclePriceComparison.dbo.AutomakerByCountry
+(Country nvarchar(50), Automaker nvarchar(50))
+
+INSERT INTO 
+
+
 
 
 
